9cfef0ca210a50e653f4a2ee543e2febd832aa77
Jose Caldas <jrcaldas@yahoo.com.br>
2023-09-27 20:34:31 -0300

    obtaining user token

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,14 +1,52 @@
-import { View } from 'react-native'
-import { Container, Slogan, Title } from './styles'
-import backgroundImg from '../../assets/background.png'
+import { useEffect, useState } from 'react'
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
 import { Button } from '../../components/Button'
+import backgroundImg from '../../assets/background.png'
+import { Container, Slogan, Title } from './styles'
+
+WebBrowser.maybeCompleteAuthSession()
 
 export function Signin() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  })
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true)
+
+    googleSignIn().then((response) => {
+      if (response.type !== 'success') {
+        setIsAuthenticating(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      if (response.authentication?.idToken) {
+        console.log(
+          'Token de Authenticacao =>',
+          response.authentication.idToken
+        )
+      }
+    }
+  }, [response])
+
   return (
     <Container source={backgroundImg}>
-      <Title>Auto Fleet</Title>
+      <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
   )
 }
