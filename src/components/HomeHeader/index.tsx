c6b6c1b2aeea076ac6f80f81aa0c9213f56a0fbc
Jose Caldas <jrcaldas@yahoo.com.br>
2023-10-02 13:32:07 -0300

    add user logout

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,12 +1,17 @@
 import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
-import { useUser } from '@realm/react'
+import { useUser, useApp } from '@realm/react'
 
 import { Container, Greeting, Message, Name, Picture } from './styles'
 import theme from '../../theme'
 
 export function HomeHeader() {
   const user = useUser()
+  const app = useApp()
+
+  function handleLogout() {
+    app.currentUser?.logOut()
+  }
 
   return (
     <Container>
@@ -15,11 +20,11 @@ export function HomeHeader() {
         placeholder="L184iAoffQof00ayfQay~qj[fQj["
       />
       <Greeting>
-        <Message>Olá</Message>
+        <Message>Olá,</Message>
         <Name>{user?.profile.name}</Name>
       </Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
