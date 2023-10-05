4e95cee920f74b82162c6785bb40c6ace34d73e2
Jose Caldas <jrcaldas@yahoo.com.br>
2023-10-05 15:13:20 -0300

    add license plate validation

--- src/components/LicensePlateInput/index.tsx
+++ src/components/LicensePlateInput/index.tsx
@@ -1,24 +1,30 @@
-import { TextInputProps } from 'react-native'
+import { TextInput, TextInputProps } from 'react-native'
 
 import { useTheme } from 'styled-components/native'
 import { Container, Input, Label } from './styles'
+import { forwardRef } from 'react'
 
 type Props = {
   label: string
 } & TextInputProps
 
-export function LicensePlateInput({ label, ...rest }: Props) {
-  const { COLORS } = useTheme()
+const LicensePlateInput = forwardRef<TextInput, Props>(
+  ({ label, ...rest }, ref) => {
+    const { COLORS } = useTheme()
 
-  return (
-    <Container>
-      <Label>{label}</Label>
-      <Input
-        maxLength={7}
-        autoCapitalize="characters"
-        placeholderTextColor={COLORS.GRAY_400}
-        {...rest}
-      />
-    </Container>
-  )
-}
+    return (
+      <Container>
+        <Label>{label}</Label>
+        <Input
+          ref={ref}
+          maxLength={7}
+          autoCapitalize="characters"
+          placeholderTextColor={COLORS.GRAY_400}
+          {...rest}
+        />
+      </Container>
+    )
+  }
+)
+
+export { LicensePlateInput }
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,24 +1,36 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import {
   ScrollView,
   TextInput,
   KeyboardAvoidingView,
   Platform,
+  Alert,
 } from 'react-native'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { LicensePlateInput } from '../../components/LicensePlateInput'
 import { TextAreaInput } from '../../components/TextAreaInput'
 import { Container, Content } from './styles'
+import { licensePlateValidate } from '../../utils/licensePlateValidate'
 
 const keyboardAvoidingViewBehavior =
   Platform.OS === 'android' ? 'height' : 'position'
 
 export function Departure() {
+  const [description, setDescription] = useState('')
+  const [licensePlate, setLicensePlate] = useState('')
+
   const descriptionRef = useRef<TextInput>(null)
+  const licensePlateRef = useRef<TextInput>(null)
 
   function handleDepartureRegister() {
-    console.log('OK!')
+    if (!licensePlateValidate(licensePlate)) {
+      licensePlateRef.current?.focus()
+      return Alert.alert(
+        'Placa inválida',
+        'A placa é inválida. Por favor, informe a placa correta do veículo.'
+      )
+    }
   }
 
   return (
@@ -28,10 +40,12 @@ export function Departure() {
         <ScrollView>
           <Content>
             <LicensePlateInput
+              ref={licensePlateRef}
               label="Placa do veículo"
               placeholder="BRA1234"
               onSubmitEditing={() => descriptionRef.current?.focus()}
               returnKeyType="next"
+              onChangeText={setLicensePlate}
             />
             <TextAreaInput
               ref={descriptionRef}
@@ -40,6 +54,7 @@ export function Departure() {
               onSubmitEditing={handleDepartureRegister}
               returnKeyType="send"
               blurOnSubmit
+              onChangeText={setDescription}
             />
 
             <Button title="Registrar saída" onPress={handleDepartureRegister} />
