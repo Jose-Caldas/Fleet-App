ddd5cecc94d3333635186d44257ecc6ecf4bf0e4
Jose Caldas <jrcaldas@yahoo.com.br>
2023-10-05 19:39:44 -0300

    add register and filter of vehicle

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -6,12 +6,19 @@ import {
   Platform,
   Alert,
 } from 'react-native'
-import { Button } from '../../components/Button'
+import { useNavigation } from '@react-navigation/native'
+
+import { useRealm } from '../../libs/realm'
+import { Historic } from '../../libs/realm/schemas/Historic'
+import { useUser } from '@realm/react'
+
 import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
 import { LicensePlateInput } from '../../components/LicensePlateInput'
 import { TextAreaInput } from '../../components/TextAreaInput'
-import { Container, Content } from './styles'
 import { licensePlateValidate } from '../../utils/licensePlateValidate'
+
+import { Container, Content } from './styles'
 
 const keyboardAvoidingViewBehavior =
   Platform.OS === 'android' ? 'height' : 'position'
@@ -19,25 +26,51 @@ const keyboardAvoidingViewBehavior =
 export function Departure() {
   const [description, setDescription] = useState('')
   const [licensePlate, setLicensePlate] = useState('')
+  const [isRegistering, setIsRegistering] = useState(false)
+
+  const { goBack } = useNavigation()
+  const realm = useRealm()
+  const user = useUser()
 
   const descriptionRef = useRef<TextInput>(null)
   const licensePlateRef = useRef<TextInput>(null)
 
   function handleDepartureRegister() {
-    if (!licensePlateValidate(licensePlate)) {
-      licensePlateRef.current?.focus()
-      return Alert.alert(
-        'Placa inválida',
-        'A placa é inválida. Por favor, informe a placa correta do veículo.'
-      )
-    }
+    try {
+      if (!licensePlateValidate(licensePlate)) {
+        licensePlateRef.current?.focus()
+        return Alert.alert(
+          'Placa inválida',
+          'A placa é inválida. Por favor, informe a placa correta do veículo.'
+        )
+      }
 
-    if (description.trim().length === 0) {
-      descriptionRef.current?.focus()
-      return Alert.alert(
-        'Finalidade',
-        'Por favor, informe a finalidade da utilização do veículo.'
-      )
+      if (description.trim().length === 0) {
+        descriptionRef.current?.focus()
+        return Alert.alert(
+          'Finalidade',
+          'Por favor, informe a finalidade da utilização do veículo.'
+        )
+      }
+      setIsRegistering(true)
+
+      realm.write(() => {
+        realm.create(
+          'Historic',
+          Historic.generate({
+            user_id: user!.id,
+            license_plate: licensePlate.toUpperCase(),
+            description,
+          })
+        )
+      })
+
+      Alert.alert('Saída', 'Saída do veículo registrada com sucesso!')
+      goBack()
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Error', 'Não foi possível registrar a saída do veículo.')
+      setIsRegistering(false)
     }
   }
 
@@ -65,7 +98,11 @@ export function Departure() {
               onChangeText={setDescription}
             />
 
-            <Button title="Registrar saída" onPress={handleDepartureRegister} />
+            <Button
+              title="Registrar saída"
+              onPress={handleDepartureRegister}
+              isLoading={isRegistering}
+            />
           </Content>
         </ScrollView>
       </KeyboardAvoidingView>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,51 @@
-import { Container, Content } from './styles'
-import { HomeHeader } from '../../components/HomeHeader'
-import { CarStatus } from '../../components/CarStatus'
+import { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
+import { useQuery } from '../../libs/realm'
+import { Historic } from '../../libs/realm/schemas/Historic'
+
+import { HomeHeader } from '../../components/HomeHeader'
+import { CarStatus } from '../../components/CarStatus'
+
+import { Container, Content } from './styles'
+import { Alert } from 'react-native'
+
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null)
   const { navigate } = useNavigation()
+
+  const historic = useQuery(Historic)
 
   function handleRegisterMovement() {
     navigate('departure')
   }
+
+  function fetchVehicle() {
+    try {
+      const vehicle = historic.filtered("status = 'departure'")[0]
+      setVehicleInUse(vehicle)
+    } catch (error) {
+      console.log(error)
+      Alert.alert(
+        'Veículo em uso',
+        'Não foi possível carregar o veículo em uso.'
+      )
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicle()
+  }, [])
 
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   )
